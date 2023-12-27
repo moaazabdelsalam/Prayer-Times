65e59020e14c08e343fe73c413ca136874d7d5a2
Moaaz AbdEl-Salam <moaazabdelsalam99@gmail.com>
2023-12-27 12:33:56 +0200

    add SchedulerUseCase to check if to load data from local or remote

--- app/src/main/java/com/task/praytimes/MainActivity.kt
+++ app/src/main/java/com/task/praytimes/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.task.praytimes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.task.praytimes.times.data.local.LocalSourceImp
 import com.task.praytimes.times.data.remote.ApiState
 import com.task.praytimes.times.data.remote.RemoteSourceImp
 import com.task.praytimes.times.data.remote.repo.RepoImp
 import com.task.praytimes.times.domain.PrayerTimesUseCase
+import com.task.praytimes.times.domain.SchedulerUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -18,16 +19,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val getPrayerTimesUseCase = PrayerTimesUseCase(
-            RepoImp.getInstance(
-                RemoteSourceImp.getInstance(),
-                LocalSourceImp.getInstance(this)
-            )
+        val repo = RepoImp.getInstance(
+            RemoteSourceImp.getInstance(),
+            LocalSourceImp.getInstance(this)
         )
+        val getPrayerTimesUseCase = PrayerTimesUseCase(repo)
+        val schedulerUseCase = SchedulerUseCase(repo, getPrayerTimesUseCase)
 
         lifecycleScope.launch(Dispatchers.IO) {
-            getPrayerTimesUseCase(2023, 12, 30.9836957, 31.1591778).collectLatest {
-                when(it) {
+            schedulerUseCase(2023, 12, 30.9836957, 31.1591778).collectLatest {
+                when (it) {
                     is ApiState.Failure -> Log.i("TAG", "failure: ${it.error}")
                     is ApiState.Loading -> Log.i("TAG", "loading")
                     is ApiState.Success -> Log.i("TAG", "success: ${it.data}")
---
+++ app/src/main/java/com/task/praytimes/times/domain/SchedulerUseCase.kt
@@ -0,0 +1,59 @@
+package com.task.praytimes.times.domain
+
+import android.util.Log
+import com.task.praytimes.times.data.remote.ApiState
+import com.task.praytimes.times.data.remote.repo.Repo
+import com.task.praytimes.times.presentation.PrayerTimes
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SchedulerUseCase(
+    private val repo: Repo,
+    private val prayerTimesUseCase: PrayerTimesUseCase
+) {
+    private val TAG = "TAG SchedulerUseCase"
+
+    operator fun invoke(
+        year: Int,
+        month: Int,
+        latitude: Double,
+        longitude: Double
+    ): Flow<ApiState<List<PrayerTimes>>> {
+        return flow {
+            emit(ApiState.Loading)
+            if (isTimePassed()) {
+                Log.i(TAG, "requesting data from network: ")
+                emit(makeNetworkCall(year, month, latitude, longitude))
+            } else {
+                Log.i(TAG, "loading local data:")
+                val localList = repo.getLocalPrayerTimes()
+                if (localList.isEmpty()) {
+                    Log.i(TAG, "empty data requesting from network: ")
+                    emit(makeNetworkCall(year, month, latitude, longitude))
+                } else {
+                    emit(ApiState.Success(localList))
+                }
+            }
+        }
+    }
+
+    private fun isTimePassed(): Boolean {
+        return false
+    }
+
+    private suspend fun makeNetworkCall(
+        year: Int,
+        month: Int,
+        latitude: Double,
+        longitude: Double
+    ): ApiState<List<PrayerTimes>> {
+        val state: ApiState<List<PrayerTimes>> =
+            prayerTimesUseCase(year, month, latitude, longitude)
+        return if (state is ApiState.Success) {
+            repo.addPrayerTimesToLocal(state.data)
+            state
+        } else {
+            state
+        }
+    }
+}
